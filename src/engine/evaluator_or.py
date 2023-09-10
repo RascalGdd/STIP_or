95d4f99018b20e2c40f7dd5dc1011788a97b6a49
Diandian Guo <guodiandian1998@163.com>
2023-09-10 16:05:38 +0200

    teset

--- src/engine/evaluator_or.py
+++ src/engine/evaluator_or.py
@@ -319,12 +319,12 @@ def or_evaluate_infer(model, postprocessors, data_loader, device, thr, args):
             obj = OBJECT_LABEL_MAP[int(inst[1])]
             verb = VERB_LABEL_MAP[int(inst[2])]
             if args.use_tricks_val:
-                if inst[2] == 3 and scores[index] < 0.08:
+                if inst[2] == 3 and scores[index] < 0.09:
                     continue
-                if inst[2] == 7 and scores[index] < 0.08:
+                if inst[2] == 7 and scores[index] < 0.075:
                     continue
                 if inst[2] == 0:
-                    if scores[index] < 0.07:
+                    if scores[index] < 0.08:
                         continue
                     else:
                         hold = False
@@ -355,11 +355,11 @@ def or_evaluate_infer(model, postprocessors, data_loader, device, thr, args):
                 if ((inst[0] not in [6, 7]) or (inst[1] != 4)) and (
                         inst[2] == 7):
                     continue
-                if ((inst[0] not in [6, 7, 8]) or (inst[1] not in [6, 7])) and (
+                if ((inst[0] not in [6, 7]) or (inst[1] not in [6, 7])) and (
                         inst[2] == 0):
                     continue
                 if (not ((inst[0] == 7 and inst[1] == 2) or (
-                        inst[0] == 8 and inst[1] == 3) or (inst[0] == 6 and inst[1] == 5) or (inst[0] == 7 and inst[1] == 5) or (inst[0] == 6 and inst[1] == 2) or (inst[0] == 6 and inst[1] == 3))) and (
+                        inst[0] == 8 and inst[1] == 3) or (inst[0] == 6 and inst[1] == 5) or (inst[0] == 7 and inst[1] == 5))) and (
                         inst[2] == 13):
                     continue
                 if (inst[0] not in [5, 6, 7, 8, 9]) and (
